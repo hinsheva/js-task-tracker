7835e442f70077680eb6e0b4b8a3bfd1afc684ba
Elena Insheva <elena.insheva@kindredgroup.com>
2021-05-04 14:49:41 +0200

    added 'delete task from the server' logic

--- src/App.js
+++ src/App.js
@@ -29,7 +29,8 @@ function App() {
     setTasks([...tasks, newTask])
   }
 
-  const deleteTask = (id) => {
+  const deleteTask = async (id) => {
+    await fetch(`http://localhost:5000/tasks/${id}`, {method: 'DELETE'});
     setTasks(tasks.filter((task)=>task.id !== id));
   }
 
